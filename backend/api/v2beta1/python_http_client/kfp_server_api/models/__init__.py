cb39924ee11952d15ad62c0542f7d2940881d1be
gkcalat <gkcalat@ucla.edu>
2022-11-23 10:58:54 -0800

    Generate api client

--- backend/api/v2beta1/python_http_client/kfp_server_api/models/__init__.py
+++ backend/api/v2beta1/python_http_client/kfp_server_api/models/__init__.py
@@ -18,15 +18,12 @@ from kfp_server_api.models.api_artifact_list import ApiArtifactList
 from kfp_server_api.models.api_cron_schedule import ApiCronSchedule
 from kfp_server_api.models.api_error import ApiError
 from kfp_server_api.models.api_experiment import ApiExperiment
-from kfp_server_api.models.api_filter import ApiFilter
 from kfp_server_api.models.api_list_experiments_response import ApiListExperimentsResponse
 from kfp_server_api.models.api_list_recurring_runs_response import ApiListRecurringRunsResponse
 from kfp_server_api.models.api_list_runs_response import ApiListRunsResponse
 from kfp_server_api.models.api_periodic_schedule import ApiPeriodicSchedule
 from kfp_server_api.models.api_pipeline_task_detail import ApiPipelineTaskDetail
 from kfp_server_api.models.api_pipeline_task_executor_detail import ApiPipelineTaskExecutorDetail
-from kfp_server_api.models.api_predicate import ApiPredicate
-from kfp_server_api.models.api_predicate_operation import ApiPredicateOperation
 from kfp_server_api.models.api_read_artifact_response import ApiReadArtifactResponse
 from kfp_server_api.models.api_recurring_run import ApiRecurringRun
 from kfp_server_api.models.api_recurring_run_status import ApiRecurringRunStatus
@@ -41,9 +38,6 @@ from kfp_server_api.models.api_runtime_status import ApiRuntimeStatus
 from kfp_server_api.models.api_status import ApiStatus
 from kfp_server_api.models.api_storage_state import ApiStorageState
 from kfp_server_api.models.api_trigger import ApiTrigger
-from kfp_server_api.models.predicate_int_values import PredicateIntValues
-from kfp_server_api.models.predicate_long_values import PredicateLongValues
-from kfp_server_api.models.predicate_string_values import PredicateStringValues
 from kfp_server_api.models.protobuf_any import ProtobufAny
 from kfp_server_api.models.protobuf_null_value import ProtobufNullValue
 from kfp_server_api.models.recurring_run_mode import RecurringRunMode
